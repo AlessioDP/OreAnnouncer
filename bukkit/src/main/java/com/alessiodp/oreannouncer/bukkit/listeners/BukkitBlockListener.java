184e44c362a3a1e6ccdc2d98a49a95d8b16cbd5b
AlessioDP <alessiodptv@gmail.com>
2019-09-09 11:20:15 +0200

    Release 2.3.1

--- bukkit/src/main/java/com/alessiodp/oreannouncer/bukkit/listeners/BukkitBlockListener.java
+++ bukkit/src/main/java/com/alessiodp/oreannouncer/bukkit/listeners/BukkitBlockListener.java
@@ -6,12 +6,15 @@ import com.alessiodp.oreannouncer.common.OreAnnouncerPlugin;
 import com.alessiodp.oreannouncer.common.configuration.OAConstants;
 import com.alessiodp.oreannouncer.common.configuration.data.ConfigMain;
 import com.alessiodp.oreannouncer.common.listeners.BlockListener;
+import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+
+import java.util.Set;
 
 public class BukkitBlockListener extends BlockListener implements Listener {
 	
@@ -22,12 +25,19 @@ public class BukkitBlockListener extends BlockListener implements Listener {
 	@EventHandler(priority=EventPriority.MONITOR)
 	public void onBlockBreak(BlockBreakEvent event) {
 		if (!event.isCancelled()) {
+			int enchantmentLevel = 0;
+			try {
+				enchantmentLevel = event.getPlayer().getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.SILK_TOUCH);
+			} catch (NoSuchMethodError ex) {
+				// MC 1.8
+				enchantmentLevel = event.getPlayer().getInventory().getItemInHand().getEnchantmentLevel(Enchantment.SILK_TOUCH);
+			}
 			super.onBlockBreak(
 					new BukkitUser(plugin, event.getPlayer()),
 					event.getBlock().getType().name(),
-					event.getPlayer().getLastTwoTargetBlocks(null,1).get(0).getLightLevel(),
+					event.getPlayer().getLastTwoTargetBlocks((Set<Material>) null,1).get(0).getLightLevel(),
 					event.getBlock().hasMetadata(OAConstants.BLOCK_METADATA),
-					event.getPlayer().getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.SILK_TOUCH) > 0,
+					enchantmentLevel > 0,
 					new ADPLocation(
 							event.getBlock().getLocation().getWorld().getName(),
 							event.getBlock().getLocation().getX(),
